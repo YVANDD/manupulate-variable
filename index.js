047ff838a6ec9e8a80096668fb0953193240cbee
nyvan80076@gmail.com <nyvan80076@gmail.com>
2023-07-05 00:29:17 +0300

    i have added some comment line

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const inputs = document.querySelectorAll(".controls input");
 
+// that function will controlling
 
 function handleUpdate(){
   const suffix = this.dataset.sizing||"";
   document.documentElement.style.setProperty(`--${this.name}`, this.value +suffix);
   // console.log(suffix);
 }
+
+// that event will be listened
+
 inputs.forEach(inn =>inn.addEventListener("change", handleUpdate));
 inputs.forEach(inn =>inn.addEventListener("mousemove", handleUpdate));
